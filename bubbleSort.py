b4ea34caff710297400be20864b404c6ecc73681
myafa <821koesnow@gmail.com>
2018-06-04 18:18:17 +0900

    bubble sort function added

---
+++ bubbleSort.py
@@ -0,0 +1,10 @@
+import random
+
+def performBubbleSort(lstNumbers):
+    for itr1 in range(0,N):
+	for itr2 in range(itr1+1, N):
+	    if lstNumbers[itr1] < lstNumbers[itr2]:
+		lstnumber[itr1],lstNumbers[itr2] = lstNumbers[itr2],lstNumbers[itr1]
+    return lstNumbers
+
+
